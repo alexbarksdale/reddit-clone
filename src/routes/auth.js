eece6d9cdbfb3aa06a45e6385abaaa0555543527
alexbarksdale <ale.barksdale@gmail.com>
2020-06-03 15:26:32 -0500

    [Add] Login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,6 +6,10 @@ const User = require('../models/user');
 const router = express.Router();
 
 // GET
+router.get('/login', (_, res) => {
+    res.render('login');
+});
+
 router.get('/sign-up', (_, res) => {
     res.render('sign-up');
 });
@@ -16,6 +20,40 @@ router.get('/logout', (_, res) => {
 });
 
 // POST
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Find this user name
+    User.findOne({ username }, 'username password')
+        .then((user) => {
+            if (!user) {
+                // User not found
+                return res.status(401).send({ message: 'Wrong Username or Password' });
+            }
+            // Check the password
+            user.comparePassword(password, (err, isMatch) => {
+                if (!isMatch) {
+                    // Password does not match
+                    return res.status(401).send({ message: 'Wrong Username or password' });
+                }
+                // Create a token
+                const token = jwt.sign(
+                    { _id: user._id, username: user.username },
+                    process.env.SECRET,
+                    {
+                        expiresIn: '60 days',
+                    }
+                );
+                // Set a cookie and redirect to root
+                res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+                res.redirect('/');
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 router.post('/sign-up', (req, res) => {
     // Create User and JWT
     const user = new User(req.body);
